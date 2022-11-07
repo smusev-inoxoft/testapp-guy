befb8b2b7c45681972b4f458e4fa51440f825ae8
Yevhenii <yevhenii.smusiev@inoxoft.com>
2022-11-07 15:41:33 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootNavigator } from './src/navigation';
-import React from 'react';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
